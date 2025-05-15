72dc28e0aae6b79b8115585c69b20fd809d406fc
Taylor Alex Bonneville <taylorbonneville@gmail.com>
2025-05-15 14:33:47 -0600

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import TopNav from "@/components/TopNav";
 import Sidebar from "@/components/Sidebar";
 import ChatWindow from "@/components/ChatWindow";
 import MessageInput from "@/components/MessageInput";
-import { useState, useRef } from "react";
 
 interface Message {
   role: "user" | "assistant";
